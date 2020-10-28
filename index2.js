125f3591acec821c3d8724d2911330271de1cbc4
DerGhoul <56272358+DerGhoul1@users.noreply.github.com>
2020-10-28 11:50:04 +0100

    Add files via upload
    updating

--- index2.js
+++ index2.js
@@ -1,7 +1,7 @@
-function selectchanger(sender){
+function selectchanger(senderID, sender){
 	var check = document.getElementsByClassName("check");
-	var sendedId = sender;
-
+	var sendedId = senderID;
+	var sendervalue = sender.value;
 
 	for(var i = 0;i<check.length;i++){
 
@@ -13,8 +13,55 @@ function selectchanger(sender){
 		}
 
 	}
+	toGrund(sendervalue);
 
 }
+
+
+function toGrund(sender){
+	var senderValue = sender;
+	document.getElementById("grund").value = senderValue;
+	
+}
+
+
+function verrechnen(value){
+	//console.log(value);
+	var grund = document.getElementById("grund").value;
+	var verteilung = document.getElementsByClassName("GeldBerechner");
+	var check = document.getElementsByClassName("check");
+	
+	for(var i = 0;i<check.length;i++){
+	
+		if(check[i].value  == grund && check[i].value == verteilung[i].name){
+			
+			console.log(verteilung[i].name);
+			var geld = parseFloat(verteilung[i].value);
+			console.log(geld);
+			var abheben = parseFloat(document.getElementById("preis").value);
+			console.log(abheben);
+			var ergebnis = geld-abheben;
+			console.log(ergebnis.toFixed(2));
+			verteilung[i].value = ergebnis.toFixed(2);
+		}
+	}
+	
+}
+
+
+function datumsformatierung(date){
+	var datesplitter = date.split("-");
+	var tag = datesplitter[2];
+	var monat = datesplitter[1];
+	var jahr = datesplitter[0];
+	var datum = (tag+"."+monat+"."+jahr);
+	
+	return(datum);
+}
+
+
+
+
 
 function disselect(){
 	var check = document.getElementsByClassName("check");
@@ -27,6 +74,7 @@ function disselect(){
 
 function wichIsSelected(){
 	var check = document.getElementsByClassName("check");
+	//var alarm = document.getElementById("hierfehltwas").innerHTML;
 
 	for(var i = 0;i<check.length;i++){
 		if(check[i].checked == true){
@@ -36,18 +84,32 @@ function wichIsSelected(){
 	}
 }
 
-
+///Noch nicht definiert
 function book(){
+	
+	
+	
+	
 	var selected = wichIsSelected();
 	console.log(selected);
 	//hier kommt ein Verbuchungsalgorithmus
 	var grund = document.getElementById("grund").value;
 	var beschreibung = document.getElementById("beschreibung").value;
 	var date = document.getElementById("date").value;
-	var preis =document.getElementById("preis").value;
-
-
-	disselect();
+	var preis = document.getElementById("preis").value;
+	var datum = datumsformatierung(date);
+	
+	if(grund == "" || beschreibung == "" || preis == "" || datum == ""){
+		document.getElementById("hierfehltwas").innerHTML = "Bitte alle Felder belegen";
+		console.log("BaFb");
+	}else{
+		console.log(grund+";"+beschreibung+";"+datum+";"+preis);
+		disselect();
+	}
+	
+	
+	
+	
 
 
 }
@@ -76,12 +138,13 @@ function umverteilung(){
 
 	var value = parseInt(lohn.value);
 
-	bild.value = 0.1 * value;
-	spas.value = 0.1 * value;
-	spar.value = 0.1 * value;
-	ruec.value = 0.1 * value;
-	inve.value = 0.1 * value;
-	notw.value = 0.5 * value;
+	bild.value = (0.1 * value).toFixed(2);
+	spas.value = (0.1 * value).toFixed(2);
+	spar.value = (0.1 * value).toFixed(2);
+	ruec.value = (0.1 * value).toFixed(2);
+	inve.value = (0.1 * value).toFixed(2);
+	notw.value = (0.5 * value).toFixed(2);
+	
 }
 
 
