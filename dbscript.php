d9ebc7918fb8c8267d7d67b2a0d2f6dbc9449982
ReinhardGSchneider <reinhardgschneider@gmail.com>
2021-06-08 19:05:57 +0200

    s

--- dbscript.php
+++ dbscript.php
@@ -1,15 +1,19 @@
 <?php
+
 $servername = "localhost";
 $username = "nas1";
 $password = "nas1";
 $dbname = "TEST";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
-if ($conn->connect_error) {
- die("Connection failed: " . $conn->connect_error);
+if ($conn -> connect_error) {
+ die( "Connection failed: " . $conn->connect_error);
 }
+
 echo "Connected successfully";
+
+
 ?>
